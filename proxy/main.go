8c04d58e0045799e9f394618e3069625ad9b157c
Mark St.Godard <markstgodard@gmail.com>
2016-12-15 16:48:06 -0800

    Pass socket path in as arg
    Signed-off-by: Gabe Rosenhouse <grosenhouse@pivotal.io>

--- proxy/main.go
+++ proxy/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
 )
 
 func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Println("mds-proxy <socket-path>")
+		os.Exit(1)
+	}
+
+	socket := os.Args[1]
 
 	ln, err := net.Listen("tcp", "127.0.0.1:5000")
 	if err != nil {
@@ -21,7 +30,7 @@ func main() {
 
 		go func(conn net.Conn) {
 			defer conn.Close()
-			client, err := net.Dial("unix", "/tmp/mds.sock")
+			client, err := net.Dial("unix", socket)
 			if err != nil {
 				panic(err)
 			}
